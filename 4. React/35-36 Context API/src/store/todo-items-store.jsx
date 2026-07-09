be4ee4bf55e3316a884ce2b71d479964ecbf2504
Ayush Solanki <0solanki.ayush@gmail.com>
2026-07-09 14:44:39 +0530

    todo using context api application

---
+++ 4. React/35-36 Context API/src/store/todo-items-store.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react"
+
+export const TodoitemsContext=createContext({
+  todoItems:[], addNewItem: ()=>{}, deleteItem: ()=>{},
+});
+
+//by declaring object in store only, it helps in auto completition of code,, functions
+
