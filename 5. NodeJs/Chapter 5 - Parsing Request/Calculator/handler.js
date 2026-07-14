d9f8a620580d4ff991d9c6338b6386365f41d5b4
Ayush Solanki <0solanki.ayush@gmail.com>
2026-07-14 11:21:07 +0530

    updated

--- 5. NodeJs/Chapter 5 - Parsing Request/Calculator/handler.js
+++ 5. NodeJs/Chapter 5 - Parsing Request/Calculator/handler.js
@@ -1,35 +1,45 @@
-const handleRequest=(req,res)=>{
-  res.setHeader("Content-type","text.html")
-  res.write('<html>')
-  res.write('<head><title>Calculator</title></head>')
-  if(req.url==='/calculator') {
-    res.write('<body>');
-    res.write('<h1>Calculator</h1>')
-    res.write('<form action="/calculate-result">')
-    res.write('<input type="text" name="a">')
-    res.write('<br><input type="text" name="b" >')
-    res.write('</form')
-    res.write('</body>');
-    res.write('</html>');
-    return res.end()
-    res.end()  
+const handleRequest = (req, res) => {
+  console.log(req.url, req.method);
+  res.setHeader("Content-type", "text.html");
+  res.write("<html>");
+  res.write("<head><title>Calculator</title></head>");
+  if (req.url === "/calculator") {
+    res.write("<body>");
+    res.write("<h1>Calculator</h1>");
+    res.write('<form action="/calculate-result" method="POST">');
+    res.write(
+      '<input type="number" name="a" placeholder="Enter number 1: "><br>',
+    );
+    res.write(
+      '<br><input type="number" name="b" placeholder="Enter number 2: ">',
+    );
+    res.write("<br><br><button type='submit'>Sum</button>");
+    res.write("</form");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+    res.end();
+  } else if (req.url === "/calculate-result" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const fullBody = Buffer.concat(body).toString();
+      console.log(fullBody);
+      const params=new URLSearchParams(fullBody)
+      const bodyObject=Object.fromEntries(params)
+      console.log(bodyObject)
+    });
   }
-  else if(req.url==='/calculate-result'){
 
-  }
+  res.write("<body>");
+  res.write("<p>Welcome</p>");
+  res.write('<br></br> <a href="/calculator">Go to Calculator</a>');
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
+};
 
-  res.write('<body>');
-  res.write('<p>Welcome</p>')
-  res.write('<br></br> <a href="/calculator">Go to Calculator</a>')
-  res.write('</body>');
-  res.write('</html>')
-  res.end()
-}
-
-
-
-
-
-
-
-module.exports=handleRequest
+module.exports = handleRequest;
